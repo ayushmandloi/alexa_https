1fc1c16617a169d8534e56eac7c066799ab763a8
ayushmandloi <ayushddon@gmail.com>
2018-01-14 11:26:02 +0530

    version beta 1.1

--- alexa_file.py
+++ alexa_file.py
@@ -8,7 +8,7 @@ import unidecode
 app = Flask(__name__)
 ask = Ask(app, "/reddit_reader")
 
-def get_headlines():
+def get_headlines(type):
     user_pass_dict = {'user': 'ayush210',
                       'passwd': 'ayush210',
                       'api_type': 'json'}
@@ -16,15 +16,14 @@ def get_headlines():
     sess.headers.update({'User-Agent': 'I am testing Alexa : Ayush'})
     sess.post('https://www.reddit.com/api/login', data=user_pass_dict)
     time.sleep(1)
-    url = 'https://reddit.com/r/gadgets/.json?limit=10'
+    url = 'https://reddit.com/r/'+type+'/.json?limit=10'
     html = sess.get(url)
     data = json.loads(html.content.decode('utf-8'))
     titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
     titles = '... '.join([i for i in titles])
     return titles
 
-news = get_headlines()
-print news
+
 
 @app.route('/')
 def homepage():
@@ -32,18 +31,36 @@ def homepage():
 
 @ask.launch
 def start_skill():
-    welcome_message = "hello there woul you like the news?"
+    welcome_message = "hello there, welcome to the reddit news, what type of news you want to hear?"
     return question(welcome_message)
 
-@ask.intent("YesIntent")
-def share_headlines():
-    headlines = get_headlines()
+# @ask.intent("YesIntent")
+# def share_headlines():
+#     headlines = get_headlines('world')
+#     headline_msg = 'The current world headlines are {}'.format(headlines)
+#     return statement(headline_msg)
+
+@ask.intent("WorldNewsIntent")
+def world_news_intent():
+    headlines = get_headlines('worldnews')
     headline_msg = 'The current world headlines are {}'.format(headlines)
+    return statement(headline_msg)
+
+@ask.intent("GadgetsNewsIntent")
+def world_news_intent():
+    headlines = get_headlines('gadgets')
+    headline_msg = 'The current gadget headlines are {}'.format(headlines)
+    return statement(headline_msg)
+
+@ask.intent("AndroidNewsIntent")
+def world_news_intent():
+    headlines = get_headlines('android')
+    headline_msg = 'The current Android headlines are {}'.format(headlines)
     return statement(headline_msg)
 
 @ask.intent("NoIntent")
 def no_intent():
-    bye_text ='I am not sure then why you have asked me to open that app...bye'
+    bye_text ='I am not sure then why you have asked me to open that app...bye!'
     return statement(bye_text)
 
 if __name__ == '__main__':
